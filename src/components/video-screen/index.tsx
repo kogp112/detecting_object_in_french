a105970f5fe0e122a153da36617d545ea9163ab7
kogp1102 <gapkana@gmail.com>
2020-12-03 21:41:42 +0900

    change appbar color

--- src/components/video-screen/index.tsx
+++ src/components/video-screen/index.tsx
@@ -7,7 +7,18 @@ import Grid from "@material-ui/core/Grid";
 import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from "@material-ui/core/Button";
-import { Container } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import orange from '@material-ui/core/colors/orange';
+import { Box } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+    }
+  },
+  spacing: 8
+});
 class VideoScreen extends React.Component<Props> {
   data: NodeJS.Timeout;
   constructor(props: Props) {
@@ -17,7 +28,7 @@ class VideoScreen extends React.Component<Props> {
     };
   }
 
-   // video settings
+  // video settings
   videoConstraints = {
     width: 350,
     height: 350,
@@ -48,44 +59,50 @@ class VideoScreen extends React.Component<Props> {
   render () {
     return (
       <>
-        <Grid container>
-          <Grid item xs={12}>
-            <AppBar position="static">
-              <Toolbar>
-                <IconButton edge="start" color="default" aria-label="menu">
-                  <MenuIcon />
-                </IconButton>
+        <ThemeProvider theme={theme}>
+          <Grid container>
+            <Grid item xs={12}>
+              <AppBar position="static">
+                <Toolbar>
+                  <IconButton edge="start" color="default" aria-label="menu">
+                    <MenuIcon />
+                  </IconButton>
+                  <Typography variant="h6">
+                    Recognition App
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid item xs={2}>
+              <Grid container>
+                <Box mt={2} ml={4}>
+                  <Button variant="contained" color="default" onClick={this.capture.bind(this)}>START</Button>
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid item xs={8} alignItems={'center'}>
+              <Grid container alignContent={'center'} justify={'center'}>
+                <Box mt={2}>
+                  <Webcam
+                    audio={false}
+                    ref={"webcam"}
+                    screenshotFormat="image/jpeg"
+                    videoConstraints={this.videoConstraints}
+                  />
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid item xs={2}>
+              <Grid container>
                 <Typography variant="h6">
-                  Recognition App
+                  Result XXXXXXX
                 </Typography>
-              </Toolbar>
-            </AppBar>
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item xs={2}>
-            <Grid container>
-              <Button variant="contained" color="default" onClick={this.capture.bind(this)}>START</Button>
+              </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={8} alignItems={'center'}>
-            <Grid container alignContent={'center'} justify={'center'}>
-              <Webcam
-                audio={false}
-                ref={"webcam"}
-                screenshotFormat="image/jpeg"
-                videoConstraints={this.videoConstraints}
-              />
-            </Grid>
-          </Grid>
-          <Grid item xs={2}>
-            <Grid container>
-              <Typography variant="h6">
-                Result XXXXXXX
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
+        </ThemeProvider>
       </>
     )
   }
